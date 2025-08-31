4b1a5078e73355f12b165633ed82b98fd2d5fd85
jungmin <jungmini0601@gmail.com>
2025-08-31 13:00:11 +0900

    Initial Commit

---
+++ src/main/java/com/sparta/match_scheduler/MatchSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.match_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchSchedulerApplication.class, args);
+    }
+
+}
